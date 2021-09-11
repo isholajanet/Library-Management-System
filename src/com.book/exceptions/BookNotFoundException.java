47e6354ec3aaf0265c775a41681c3ae59ff9fa56
Janet Ishola <janetishola2010@gmail.com>
2021-09-11 09:15:17 +0100

    add books, remove books

---
+++ src/com.book/exceptions/BookNotFoundException.java
@@ -0,0 +1,21 @@
+package com.book.exceptions;
+
+
+
+public class BookNotFoundException extends Exception {
+    public BookNotFoundException() {
+        super();
+    }
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+
+    public BookNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
